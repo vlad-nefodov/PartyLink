2bb23259d51d65981566031be0e4464c533274c6
VladCramer <nefedovv77@gmail.com>
2023-05-13 14:28:05 +0300

    Adding icon and customizing header

--- PartyLink.WebUI/party-link-main/src/components/Navbar/Navbar.tsx
+++ PartyLink.WebUI/party-link-main/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import './Navbar.scss';
 import { IoNotificationsOutline } from "react-icons/io5";
-import { FaBookmark, FaUserCircle, FaFire, FaSignInAlt } from "react-icons/fa";
+import { FaMap, FaBookmark, FaUserCircle, FaFire, FaSignInAlt } from "react-icons/fa";
 import { useQuery } from 'react-query';
-import { Navbar as BNavbar, Container, NavDropdown, Row, Col, Image } from 'react-bootstrap';
+import { Navbar as BNavbar, Container, NavDropdown, Image } from 'react-bootstrap';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
@@ -41,31 +41,29 @@ const Navbar = () => {
 
   return (
     <BNavbar bg='light' className='navbar-custom'>
-      <Container>
-        <BNavbar.Brand className='navbar-custom-brand' onClick={goToMap}>PartyLink</BNavbar.Brand>
-        <BNavbar.Collapse className='justify-content-end'>
-          <Row className='justify-content-end align-items-center'>
-            <Col xs="auto" className='p-0'>
-              <IoNotificationsOutline size="24px" className='navbar-custom-icon' />
-            </Col>
-            <Col xs="auto">
-              <NavDropdown title={<ProfileImage />} align={{ xxl: "start" }}>
-                <NavDropdown.Item className='navbar-dropdown-item' onClick={goToProfile}>
-                  <FaUserCircle className='navbar-dropdown-item-icon' />Profile
-                </NavDropdown.Item>
-                <NavDropdown.Item className='navbar-dropdown-item'>
-                  <FaFire className='navbar-dropdown-item-icon' />My events
-                </NavDropdown.Item>
-                <NavDropdown.Item className='navbar-dropdown-item'>
-                  <FaBookmark className='navbar-dropdown-item-icon' />Bookmarks
-                </NavDropdown.Item>
-                <NavDropdown.Item className='navbar-dropdown-item' onClick={showModal}>
-                  <FaSignInAlt className='navbar-dropdown-item-icon' />Log out
-                </NavDropdown.Item>
-                <LogoutModal show={isShowModal} onCancel={hideModal} onSubmit={logoutHandle} />
-              </NavDropdown>
-            </Col>
-          </Row>
+      <Container fluid className='ps-5 pe-5'>
+        <BNavbar.Brand className='navbar-custom-brand' onClick={goToMap}>
+          <FaMap className='mb-1 me-2' />PartyLink
+        </BNavbar.Brand>
+        <BNavbar.Collapse className='justify-content-end align-items-center'>
+          <BNavbar.Text className='navbar-custom-icon me-2 p-0'>
+            <IoNotificationsOutline size="24px" />
+          </BNavbar.Text>
+          <NavDropdown title={<ProfileImage />} align="end">
+            <NavDropdown.Item className='navbar-dropdown-item' onClick={goToProfile}>
+              <FaUserCircle className='navbar-dropdown-item-icon' />Profile
+            </NavDropdown.Item>
+            <NavDropdown.Item className='navbar-dropdown-item'>
+              <FaFire className='navbar-dropdown-item-icon' />My events
+            </NavDropdown.Item>
+            <NavDropdown.Item className='navbar-dropdown-item'>
+              <FaBookmark className='navbar-dropdown-item-icon' />Bookmarks
+            </NavDropdown.Item>
+            <NavDropdown.Item className='navbar-dropdown-item' onClick={showModal}>
+              <FaSignInAlt className='navbar-dropdown-item-icon' />Log out
+            </NavDropdown.Item>
+            <LogoutModal show={isShowModal} onCancel={hideModal} onSubmit={logoutHandle} />
+          </NavDropdown>
         </BNavbar.Collapse>
       </Container>
     </BNavbar>
--- PartyLink.WebUI/party-link-main/src/pages/MapPage/MapPage.tsx
+++ PartyLink.WebUI/party-link-main/src/pages/MapPage/MapPage.tsx
@@ -1,4 +1,6 @@
 import './MapPage.scss';
+import styles from './styles';
+import { Container } from 'react-bootstrap';
 import { GoogleMap, MarkerF, useLoadScript } from '@react-google-maps/api';
 import LoadingContainer from '../../components/LoadingContainer/LoadingContainer';
 
@@ -11,244 +13,11 @@ function MapPage() {
     return <LoadingContainer />;
   }
 
-  const styles: google.maps.MapTypeStyle[] = [
-    {
-      "featureType": "administrative",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#6195a0"
-        }
-      ]
-    },
-    {
-      "featureType": "administrative.province",
-      "elementType": "geometry.stroke",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "landscape",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "lightness": "0"
-        },
-        {
-          "saturation": "0"
-        },
-        {
-          "color": "#f5f5f2"
-        },
-        {
-          "gamma": "1"
-        }
-      ]
-    },
-    {
-      "featureType": "landscape.man_made",
-      "elementType": "all",
-      "stylers": [
-        {
-          "lightness": "-3"
-        },
-        {
-          "gamma": "1.00"
-        }
-      ]
-    },
-    {
-      "featureType": "landscape.natural.terrain",
-      "elementType": "all",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "all",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {
-          "color": "#bae5ce"
-        },
-        {
-          "visibility": "on"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "all",
-      "stylers": [
-        {
-          "saturation": -100
-        },
-        {
-          "lightness": 45
-        },
-        {
-          "visibility": "simplified"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "all",
-      "stylers": [
-        {
-          "visibility": "simplified"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {
-          "color": "#fac9a9"
-        },
-        {
-          "visibility": "simplified"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "labels.text",
-      "stylers": [
-        {
-          "color": "#4e4e4e"
-        }
-      ]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#787878"
-        }
-      ]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "labels.icon",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "transit",
-      "elementType": "all",
-      "stylers": [
-        {
-          "visibility": "simplified"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.station.airport",
-      "elementType": "labels.icon",
-      "stylers": [
-        {
-          "hue": "#0a00ff"
-        },
-        {
-          "saturation": "-77"
-        },
-        {
-          "gamma": "0.57"
-        },
-        {
-          "lightness": "0"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.station.rail",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#43321e"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.station.rail",
-      "elementType": "labels.icon",
-      "stylers": [
-        {
-          "hue": "#ff6c00"
-        },
-        {
-          "lightness": "4"
-        },
-        {
-          "gamma": "0.75"
-        },
-        {
-          "saturation": "-68"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "all",
-      "stylers": [
-        {
-          "color": "#eaf6f8"
-        },
-        {
-          "visibility": "on"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "geometry.fill",
-      "stylers": [
-        {
-          "color": "#c7eced"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "lightness": "-49"
-        },
-        {
-          "saturation": "-53"
-        },
-        {
-          "gamma": "0.79"
-        }
-      ]
-    }
-  ];
-
   const center: google.maps.LatLngLiteral = { lat: 49.842957, lng: 24.031111 };
   const options: google.maps.MapOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     disableDefaultUI: true,
+    zoomControl: true,
     restriction: {
       latLngBounds: {
         north: 85,
@@ -265,9 +34,13 @@ function MapPage() {
   }
 
   return (
-    <GoogleMap center={center} mapContainerClassName="map-container" options={options}>
-      <MarkerF position={center} />
-    </GoogleMap>
+    <div className="map-container">
+      <Container className="sidebar-container">
+      </Container>
+      <GoogleMap mapContainerClassName='map-custom' center={center} options={options}>
+        <MarkerF position={center} />
+      </GoogleMap>
+    </div>
   );
 }
 
